32e1a2c0c9fd68304b86d999a3dd23b914d4521d
sumeetsingh39 <sumeetsngh0@gmail.com>
2025-12-07 20:09:17 +0530

    Setting up Github Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sumeetsingh39.github.io/",
+  base: "/portfolio-system",
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha",
